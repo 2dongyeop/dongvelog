e984ccefd856c447c837ff081f32d9fc857eecbf
leedongyeop <ldy_1204@naver.com>
2023-06-20 16:14:49 +0900

    test : RestDocs 의존성 추가 및 테스트 작성

---
+++ src/test/java/com/dongvelog/controller/PostControllerDocTest.java
@@ -0,0 +1,97 @@
+package com.dongvelog.controller;
+
+
+import com.dongvelog.domain.post.controller.request.CreatePostRequest;
+import com.dongvelog.domain.post.entity.Post;
+import com.dongvelog.domain.post.repository.PostRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "api.dongvelog.com", uriPort = 443)
+@ExtendWith(RestDocumentationExtension.class)
+public class PostControllerDocTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private PostRepository postRepository;
+
+
+    @Test
+    @DisplayName("단건 조회 테스트")
+    public void test1() throws Exception {
+        //given -- 조건
+        final Post post = Post.builder()
+                .title("제목")
+                .content("내용")
+                .build();
+        postRepository.save(post);
+
+        //expected
+        this.mockMvc.perform(get("/posts/{postId}", 1L)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("index",
+                        pathParameters(
+                                parameterWithName("postId").description("게시글 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("게시글 ID"),
+                                fieldWithPath("title").description("제목"),
+                                fieldWithPath("content").description("내용")
+                        )
+                ));
+    }
+
+
+    @Test
+    @DisplayName("글 등록")
+    public void test2() throws Exception {
+        //given -- 조건
+        final CreatePostRequest request = CreatePostRequest.builder()
+                .title("제목입니다.")
+                .content("내용입니다.")
+                .build();
+
+        final String jsonRequest = objectMapper.writeValueAsString(request);
+
+        //expected
+        this.mockMvc.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("index",
+                        requestFields(
+                            fieldWithPath("title").description("제목"),
+                            fieldWithPath("content").description("내용")
+                        )
+                ));
+    }
+}
