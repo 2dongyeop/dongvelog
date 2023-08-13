d7a16ff2d7b6d6f9b3df031dbedab08a04d27389
leedongyeop <ldy_1204@naver.com>
2023-08-13 22:36:01 +0900

    feat : 시큐리티 폼 로그인 커스텀

--- src/main/java/com/dongvelog/global/config/SecurityConfig.java
+++ src/main/java/com/dongvelog/global/config/SecurityConfig.java
@@ -5,12 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 public class SecurityConfig {
 
     @Bean
@@ -25,11 +31,39 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests()
-                .requestMatchers("/auth/login").permitAll()
-                .anyRequest().authenticated()
+                    .requestMatchers("/auth/login").permitAll()
+                    .anyRequest().authenticated()
 
                 .and()
+                .formLogin()
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                    .loginPage("/auth/login")
+                    .loginProcessingUrl("/auth/login")
+                    .defaultSuccessUrl("/")
+
+                .and()
+                .userDetailsService(userDetailsService())
                 .csrf().disable()
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        UserDetails user = User.withUsername("dongvelop")
+                .password("test")
+                .roles("ADMIN")
+                .build();
+
+        manager.createUser(user);
+
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
